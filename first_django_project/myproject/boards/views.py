e7d1ab275d603868f7063f63a21c8c7af6d7edd4
HoratiuLazar <horatiulazar2003@gmail.com>
2025-03-24 17:46:57 +0200

    adding bootstrap and playing with admin funtionality

--- first_django_project/myproject/boards/views.py
+++ first_django_project/myproject/boards/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, world')
-
-# Create your views here.
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
