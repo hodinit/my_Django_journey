ed9ed3d01327c79f6bfd0657f8d30230cb744c60
HoratiuLazar <horatiulazar2003@gmail.com>
2025-04-04 19:20:08 +0300

    adding reply functionality and modifying the reples and views counter

--- first_django_project/myproject/myproject/urls.py
+++ first_django_project/myproject/myproject/urls.py
@@ -34,6 +34,8 @@ urlpatterns = [
     path('logout/', custom_logout, name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new', views.new_topic, name= 'new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('admin/', admin.site.urls),
 
     path('reset/', 
@@ -57,4 +59,6 @@ urlpatterns = [
         name = 'password_change'),
     path('settings/password/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'password_change_done.html'),
         name = 'password_change_done'),
+
+    
 ]
