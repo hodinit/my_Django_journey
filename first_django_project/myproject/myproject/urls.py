cbde2754dbebb6121201e107d16c18143ace0f33
HoratiuLazar <horatiulazar2003@gmail.com>
2025-04-08 18:50:21 +0300

    pagination reusable template

--- first_django_project/myproject/myproject/urls.py
+++ first_django_project/myproject/myproject/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
     path('boards/<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('boards/<int:pk>/new', views.new_topic, name= 'new_topic'),
-    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.PostListView.as_view(), name='topic_posts'),
     path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('boards/<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/', 
         views.PostUpdateView.as_view(), name='edit_post'),
